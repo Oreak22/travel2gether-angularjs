4ddfefd7d73dee97272bb6f0bcd5cb2f53f6e4cb
Rejoice Obe <rejoiceobe235@gmail.com>
2026-08-11 20:37:29 +0100

    feat: add server route for editing admin packages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/admin/packages/:id/edit',
+    renderMode: RenderMode.Server, 
+  },
+  {
     path: 'packages/:id',
     renderMode: RenderMode.Prerender,
     getPrerenderParams: async () => [{ id: 'alpine-retreat' }, { id: 'coastal-escape' }],
